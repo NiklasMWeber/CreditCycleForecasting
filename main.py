e7bfa26bb3ddac582c0374df91c4b8449a3868c0
Niklas Weber <niklas.m.weber@gmail.com>
2021-08-06 16:00:46 +0200

    shift stuff into tools and first regression

--- main.py
+++ main.py
@@ -5,33 +5,66 @@ Created on Thu Aug  5 11:22:08 2021
 @author: Niklas Weber
 """
 
+import tools
 import numpy as np
 from esig import tosig as ts
-import tools
+import sklearn.preprocessing as pre
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
-"""
-Test computing signature and logsig:
+# =============================================================================
+# Test computing signature and logsig:
+# 
+# stream = np.array([[1.0,1.0], [3.0,4.0],[5.0,2.0],[8.0,6.0]])
+#                    
+# depth = 3
+# 
+# sig = ts.stream2sig(stream,depth)
+# logsig = ts.stream2logsig(stream,depth)
+# print(ts.sigdim(2,3))
+# print(sig) 
+# print("\n",logsig)
+# =============================================================================
 
-stream = np.array([[1.0,1.0], [3.0,4.0],[5.0,2.0],[8.0,6.0]])
-                   
-depth = 3
+# Import Data
+x_1,x_2,x_3,x_4_1,x_4_2,y = tools.importData()
+X,Y,year = tools.prepareData(x_1,x_2,x_3,x_4_1,x_4_2,y)
 
-sig = ts.stream2sig(stream,depth)
-logsig = ts.stream2logsig(stream,depth)
-print(sig) 
-print("\n",logsig)
-"""
+# Standardize Data
+scaler = pre.StandardScaler()
+X_scaled = scaler.fit_transform(X) #-mean() --> /std
+max_abs_scaler = pre.MaxAbsScaler()
+Y_scaled = max_abs_scaler.fit_transform(Y-np.mean(Y))# -mean --> range [-1,1]
 
-gdpPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\gdp_growth_north_america.csv'
-unemplPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\unemployment_north_america.csv'
-indexPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\s&p500.txt'
-stirPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\short_term_us.csv'
-ltirPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\long_term_us.csv'
-pdPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\PD_North America.csv'
 
-x_1 = tools.importFile(gdpPath, delimiter = ';')
-x_2 = tools.importFile(unemplPath, delimiter = ';')
-x_3 = tools.importFile(indexPath, delimiter = '\t')
-x_4_1 = tools.importFile(stirPath, delimiter = ',')
-x_4_2 = tools.importFile(ltirPath, delimiter = ',')
-y = tools.importFile(pdPath, delimiter = ',')
+
+'''
+# Split in train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X_scaled,Y_scaled, test_size = 0.3)
+
+# Train and fit ridge regression
+ridge_reg = Ridge(alpha = 0, fit_intercept=True)
+ridge_reg.fit(X_train,Y_train)
+
+predict_test = ridge_reg.predict(X_test)
+predict_train = ridge_reg.predict(X_train)
+
+# Plot
+fig = plt.figure()
+fig1 = fig.add_subplot(2,1,1)
+xAxes = range(1,np.shape(predict_test)[0]+1)
+fig1.plot(xAxes, Y_test, label = 'true')
+fig1.plot(xAxes, predict_test, label = 'predict')
+fig1.legend()
+fig1.set_title('Testing Set')
+print('Test R = ', ridge_reg.score(X_test, Y_test))
+
+fig2 = fig.add_subplot(2,1,2)
+xAxes = range(1,np.shape(predict_train)[0]+1)
+fig2.plot(xAxes, Y_train, label = 'true')
+fig2.plot(xAxes, predict_train, label = 'predict')
+fig2.legend()
+fig2.set_title('Training Set')
+print('Train R = ', ridge_reg.score(X_train, Y_train))
+'''
--- tools.py
+++ tools.py
@@ -1,13 +1,77 @@
 import csv
+import numpy as np
+from statistics import mean
+from itertools import groupby
 
 def importData(string):
     print('Hello '+string)
     
 def importFile(fileWithPath, delimiter):
     x = []
-    i=0
     with open(fileWithPath, 'r') as file:
         file = csv.reader(file, delimiter=delimiter)
         for row in file:
             x.append(row)
     return x
+
+def importData():
+    ''' 
+    Maybe paths as input?
+    '''
+    gdpPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\gdp_growth_north_america.csv'
+    unemplPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\unemployment_north_america.csv'
+    indexPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\s&p500.txt'
+    stirPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\short_term_us.csv'
+    ltirPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\long_term_us.csv'
+    pdPath = r'C:\Users\nikth\Dropbox\MA Gonon\Anwendung\NA_Data\PD_North America.csv'
+    
+    x_1 = importFile(gdpPath, delimiter = ';')
+    x_2 = importFile(unemplPath, delimiter = ';')
+    x_3 = importFile(indexPath, delimiter = '\t')
+    x_4_1 = importFile(stirPath, delimiter = ',')
+    x_4_2 = importFile(ltirPath, delimiter = ',')
+    y = importFile(pdPath, delimiter = ',')
+    
+    return x_1,x_2,x_3,x_4_1,x_4_2,y
+
+def prepareData(x_1,x_2,x_3,x_4_1,x_4_2,y):
+    ''' 
+    Maybe years as input?
+    '''
+    y_min = 1991
+    y_max = 2019
+    
+    #just filter for right years
+    x_1 = np.array([x for x in x_1 if (float(x[0])>=y_min and float(x[0])<=y_max)]).astype(np.float64)
+    #just filter for right years
+    x_2 = np.array([x for x in x_2 if (float(x[0])>=y_min and float(x[0])<=y_max)]).astype(np.float64)
+    #extract dates -> average years -> take right years
+    x_3 = [[x[0].split()[2],float(x[1].replace(',',''))] for x in x_3] #now its a list with [[year,value] possibly with several entries for every year (months,days...)]
+    x_3 = np.array([[key, mean(map(lambda x:x[1],list(group)))]for key, group in groupby(x_3, lambda x: x[0]) if (y_min <= float(key) and float(key) <=y_max)])
+    x_3 = np.flip(x_3,0).astype(np.float64)
+    
+    x_3_sqared = np.square(x_3)
+    
+    # extract date and val -> find intersection for dates -> calc diff -> average year-wise
+    x_4_1 = [[x[-3],x[-2]] for x in x_4_1[1:]]
+    x_4_2 = [[x[-3],x[-2]] for x in x_4_2[1:]]
+    
+    dates_1 = [x[0] for x in x_4_1]
+    dates_2 = [x[0] for x in x_4_2]
+    x_4_1 = [[x[0],x[1]] for x in x_4_1 if x[0] in dates_2]
+    x_4_2 = [[x[0],x[1]] for x in x_4_2 if x[0] in dates_1]
+    
+    x_4 = [[x[0], float(y[1])-float(x[1])] for x,y in zip(x_4_1,x_4_2)]
+    x_4 = [[x[0].split('-')[0],x[1]] for x in x_4]
+    x_4 = np.array([[key, mean(map(lambda x:x[1],list(group)))]for key, group in groupby(x_4, lambda x: x[0]) if (y_min <= float(key) and float(key) <=y_max)])
+    x_4 = x_4.astype(np.float64)
+    
+    y = [[y[0].split('-')[0],float(y[4])]for y in y[1:]]
+    y = np.array([[key, mean(map(lambda x:x[1],list(group)))]for key, group in groupby(y, lambda x: x[0]) if (y_min <= float(key) and float(key) <=y_max)])
+    
+    Y= y[:,1].astype(np.float64).reshape(-1, 1)
+    X = np.array([x_1[:,1],x_2[:,1], x_3[:,1]\
+                  ,x_3_sqared[:,1]\
+                      ,x_4[:,1]]).transpose()
+    years = y[:,0].astype(np.int64).reshape(-1, 1)
+    return X,Y,years
